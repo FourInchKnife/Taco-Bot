53e99c70ac941d02b4af9b6603c1345eba354d92
FourInchKnife <riley.j.mclain@gmail.com>
2020-09-07 19:59:58 -0400

    yeet

--- run.py
+++ run.py
@@ -1,15 +1,28 @@
 import discord
-import json
-with open("key.json","r") as file:
-    token = json.loads(file.read())[0]
 
 class Botto(discord.Client):
+    def __init__(self):
+        super().__init__()
+        self.owner=600130839870963725
+    async def shutdown(self,message):
+        if message.author.id != self.owner:
+            await message.channel.send("No.")
+            return
+        await self.close()
     async def on_ready(self):
         await self.change_presence(status=discord.Status.invisible)
         print("Logged in as {}".format(str(self.user.name)))
     async def on_message(self, message):
-        if "taco" in message.content.lower() or "🌮" in message.content:
-            await message.add_reaction("🌮")
+        if message.author.id == self.user.id:
+            return
+        if message.content.startswith("!shutdown"):
+            await self.shutdown(message)
+        elif message.content.startswith("!phaseup"):
+            await self.phaseup(message)
+        else:
+            if "taco" in message.content:
+                await message.add_reaction("🌮")
+
 
 bot = Botto()
 bot.run(token)
